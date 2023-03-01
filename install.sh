b8260dc2372df78dd0250dd8da0fb2c0441553d1
Justin Walz <justin.walz10@gmail.com>
2023-02-28 17:07:24 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,6 +7,7 @@ sudo apt-get update
 sudo apt-get -y install --no-install-recommends apt-utils dialog 2>&1
 sudo apt-get install -y \
   apt-transport-https \
+  build-essential \
   curl \
   git \
   jq \
@@ -22,5 +23,13 @@ sudo apt-get autoremove -y
 sudo rm -rf /var/lib/apt/lists/*
 
 BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-
 ln -s ${BASEDIR}/.tmux.conf ~/.tmux.conf
+
+# install homebrew
+NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+brew install \
+  gcc \
+  tig
+
+(echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> ~/.profile
